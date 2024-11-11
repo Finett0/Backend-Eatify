447f03c8750a86f86f58fda19f363f8ce82d85cd
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2024-11-11 20:13:44 -0300

    Update app.py

--- Eatify/app.py
+++ Eatify/app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = []
+
 def nome_programa():
     print("""
 
@@ -23,19 +25,47 @@ def finalizar_app():
     os.system('cls')
     print('Aplicação Encerrada!\n')
 
+def opcao_invalida():
+    print('Opção Invalida!\n')
+    input('Digite qualquer tecla para voltar ao menu')
+    main()
+
+def cadastrar_restaurante():
+    os.system('cls')
+    print('Castro de novos restaurantes\n')
+    nome_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_restaurante)
+    print(f'O restaurante {nome_restaurante} foi cadastrado com sucesso!')
+    input('Digite uma tecla para voltar ao menu principal: ')
+    main()
+
+
 def escolher_opcao():
-    opcao = int(input('Escolha uma opção: '))
-    if opcao == 1:
-        cadastrar_restaurante = input('Digite o nome do restaurente: ')
-    elif opcao == 2:
-        print('Listar restaurantes: ')
-    elif opcao == 3:
-        print('Qual restaurante deseja ativar: ')
-    else:
-        finalizar_app()
+    try:
+        opcao = int(input('Escolha uma opção: '))
+        if opcao == 1:
+            cadastrar_restaurante()
+        elif opcao == 2:
+            print('Listar restaurantes: ')
+        elif opcao == 3:
+            print('Qual restaurante deseja ativar: ')
+        elif opcao == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
 
 def main():
+    os.system('cls')
     nome_programa()
+    opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
+
     opcoes()
     escolher_opcao()
 
